fd72fdcb2dd9a814b1e50d16ca34985edf2fd1c8
Stott, Cameron <cstott@it-cstott-imac.ocad.ca>
2016-08-16 11:07:43 -0400

    Initial Commit

---
+++ Bootchoice/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Bootchoice
+//
+//  Created by Stott, Cameron on 8/16/16.
+//  Copyright © 2016 Stott, Cameron. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
