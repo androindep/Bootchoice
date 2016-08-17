7378ef6c40be674ece8739de4b63c6877bf5ddec
androindep <cameron.alex.stott@gmail.com>
2016-08-17 13:03:15 -0400

    Update AppDelegate.swift

--- Bootchoice/AppDelegate.swift
+++ Bootchoice/AppDelegate.swift
@@ -15,8 +15,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     @IBOutlet var window: NSWindow!
     
-    func orderFrontRegardless(){}
-    func makeMainWindow(){}
+
     
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
@@ -24,8 +23,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         window.level = Int(CGWindowLevelForKey(.MainMenuWindowLevelKey))
         window.canBecomeVisibleWithoutLogin = true
         window.canHide = false
-        func orderFrontRegardless(){}
-        func makeMainWindow(){}
+
     }
     
     func applicationWillTerminate(aNotification: NSNotification) {
