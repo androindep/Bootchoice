363b653b854299db50304269f5a054f855e6708b
androindep <cameron.alex.stott@gmail.com>
2016-08-25 10:58:24 -0400

    Updated AppDelegate to run 2 extra shell scripts at earlier phases
    -Run init.sh at load to detect alternate boot partitions, capture output, close if nothing detected
    -Run setvals.sh to determine plist values at runtime, removes need for postinstall script, makes partition detection dynamic

--- Bootchoice/AppDelegate.swift
+++ Bootchoice/AppDelegate.swift
@@ -2,21 +2,91 @@
 //  AppDelegate.swift
 //  Bootchoice
 //
-//  Created by Stott, Cameron on 8/16/16.
-//  Copyright © 2016 Stott, Cameron. All rights reserved.
+//  Created by C. Stott on 8/16/16.
+//  Copyright © 2016 C. Stott. All rights reserved.
 //
 
 import Cocoa
 
 
+
 @NSApplicationMain
 
+
+    
 class AppDelegate: NSObject, NSApplicationDelegate {
+ 
+    func runCommand(cmd : String, args : String...) -> (output: [String], error: [String], exitCode: Int32) {
+        
+        var output : [String] = []
+        var error : [String] = []
+        
+        let task = NSTask()
+        task.launchPath = cmd
+        task.arguments = args
+        
+        let outpipe = NSPipe()
+        task.standardOutput = outpipe
+        let errpipe = NSPipe()
+        task.standardError = errpipe
+        
+        task.launch()
+        
+        let outdata = outpipe.fileHandleForReading.readDataToEndOfFile()
+        if var string = String.fromCString(UnsafePointer(outdata.bytes)) {
+            string = string.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
+            output = string.componentsSeparatedByString("\n")
+        }
+        
+        let errdata = errpipe.fileHandleForReading.readDataToEndOfFile()
+        if var string = String.fromCString(UnsafePointer(errdata.bytes)) {
+            string = string.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
+            error = string.componentsSeparatedByString("\n")
+        }
+        
+        task.waitUntilExit()
+        let status = task.terminationStatus
+        
+        return (output, error, status)
+    }
+    
+    func applicationWillFinishLaunching(notification: NSNotification) {
+        
+        let (output, error, status) = runCommand("/Applications/Utilities/Bootchoice.app/Contents/Resources/init.sh")
+        print("program exited with status \(status)")
+        let initstatus = (status)
+        if initstatus == 1 {
+
+            NSApplication.sharedApplication().terminate(self)
+        }
+        if output.count > 0 {
+            print("program output:")
+            print(output)
+        }
+        if error.count > 0 {
+
+            NSApplication.sharedApplication().terminate(self)
+        }
+        
+
+        
+    }
+
     
     @IBOutlet var window: NSWindow!
     
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        
+        let setscript = "/Applications/Utilities/Bootchoice.app/Contents/Resources/setvals.sh"
+        
+        let settask = NSTask()
+        settask.launchPath = "/bin/bash"
+        settask.arguments = ["-c", setscript]
+        settask.launch()
+        settask.waitUntilExit()
+        
         window.makeKeyAndOrderFront(NSWindow)
         window.orderFrontRegardless()
         window.level = Int(CGWindowLevelForKey(.MainMenuWindowLevelKey))
@@ -29,19 +99,22 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
         
     }
+    
 
     @IBAction func MacOS(sender: NSButton) {
+        sender.highlighted = true
         self.window?.close()
         NSApplication.sharedApplication().terminate(self)
 
     }
     @IBAction func Windows(sender: NSButton) {
-        let script = "/Applications/Utilities/Bootchoice.app/Contents/Resources/bootchoice.sh"
         
-        let task = NSTask()
-        task.launchPath = "/bin/bash"
-        task.arguments = ["-c", script]
-        task.launch()
+        let choicescript = "/Applications/Utilities/Bootchoice.app/Contents/Resources/bootchoice.sh"
+        
+        let choicetask = NSTask()
+        choicetask.launchPath = "/bin/bash"
+        choicetask.arguments = ["-c", choicescript]
+        choicetask.launch()
     }
 }
 
